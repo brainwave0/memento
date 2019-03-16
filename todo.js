a1d405ab342ea86efee6d3c859bfbd0e341ddf28
Ian Campos <iancampos.0@gmail.com>
2019-03-16 13:38:32 -0700

    update

--- todo.js
+++ todo.js
@@ -42,7 +42,6 @@ function startStop() {
     } else {
       timerDuration = (timerMin + timerMax) / 2
     }
-    throw String(waitTime) + " " + String(nice) + "!"
     AndroidAlarm.timer(timerDuration, name, false)
     entry().set("lastDuration", timerDuration)
   }
